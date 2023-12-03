485b24b202644a8bfc1975effd1e133b56acf2ea
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-12-03 15:24:42 +0100

    move out

--- 2023/day03/src/lib.rs
+++ 2023/day03/src/lib.rs
@@ -1,6 +1,5 @@
 mod p2faster_hash;
-
-use std::collections::HashMap;
+mod p2basic;
 
 use helper::Day;
 
@@ -16,7 +15,7 @@ helper::define_variants! {
         basic => crate::part1;
     }
     part2 {
-        basic => crate::part2;
+        basic => crate::p2basic::part2;
         fasher_hash => crate::p2faster_hash::part2;
     }
 }
@@ -72,63 +71,6 @@ fn part1(input: &str) -> u64 {
     acc
 }
 
-fn part2(input: &str) -> u64 {
-    fn contains_gear(s: &str) -> Option<usize> {
-        s.chars().position(|c| !c.is_ascii_digit() && c != '.')
-    }
-
-    let len = input.lines().next().unwrap().len();
-    let empty_border = std::iter::repeat('.').take(len).collect::<String>();
-
-    let mut prev2 = empty_border.as_str();
-    let mut prev1 = input.lines().next().unwrap();
-
-    let mut gears: HashMap<(usize, usize), Vec<u64>> = HashMap::new();
-
-    for (line_number, next) in input
-        .lines()
-        .skip(1)
-        .chain(Some(empty_border.as_str()))
-        .enumerate()
-    {
-        let mut numbers = prev1.char_indices().peekable();
-        while let Some((start, c)) = numbers.next() {
-            if c.is_ascii_digit() {
-                let mut end = start;
-                while let Some((idx, '0'..='9')) = numbers.next() {
-                    end = idx;
-                }
-
-                let box_bounds = (start.saturating_sub(1))..std::cmp::min(end + 2, len);
-                let number = prev1[start..=end].parse::<u64>().unwrap();
-
-                if let Some(position) = contains_gear(&prev2[box_bounds.clone()]) {
-                    let key = (line_number - 1, box_bounds.start + position);
-                    gears.entry(key).or_default().push(number);
-                }
-
-                if let Some(position) = contains_gear(&prev1[box_bounds.clone()]) {
-                    let key = (line_number, box_bounds.start + position);
-                    gears.entry(key).or_default().push(number);
-                }
-
-                if let Some(position) = contains_gear(&next[box_bounds.clone()]) {
-                    let key = (line_number + 1, box_bounds.start + position);
-                    gears.entry(key).or_default().push(number);
-                }
-            }
-        }
-
-        prev2 = prev1;
-        prev1 = next;
-    }
-
-    gears
-        .values()
-        .filter(|v| v.len() == 2)
-        .map(|v| v[0] * v[1])
-        .sum()
-}
 
 helper::tests! {
     day03 Day03;
---
+++ 2023/day03/src/p2basic.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+pub fn part2(input: &str) -> u64 {
+    fn contains_gear(s: &str) -> Option<usize> {
+        s.chars().position(|c| !c.is_ascii_digit() && c != '.')
+    }
+
+    let len = input.lines().next().unwrap().len();
+    let empty_border = std::iter::repeat('.').take(len).collect::<String>();
+
+    let mut prev2 = empty_border.as_str();
+    let mut prev1 = input.lines().next().unwrap();
+
+    let mut gears: HashMap<(usize, usize), Vec<u64>> = HashMap::new();
+
+    for (line_number, next) in input
+        .lines()
+        .skip(1)
+        .chain(Some(empty_border.as_str()))
+        .enumerate()
+    {
+        let mut numbers = prev1.char_indices().peekable();
+        while let Some((start, c)) = numbers.next() {
+            if c.is_ascii_digit() {
+                let mut end = start;
+                while let Some((idx, '0'..='9')) = numbers.next() {
+                    end = idx;
+                }
+
+                let box_bounds = (start.saturating_sub(1))..std::cmp::min(end + 2, len);
+                let number = prev1[start..=end].parse::<u64>().unwrap();
+
+                if let Some(position) = contains_gear(&prev2[box_bounds.clone()]) {
+                    let key = (line_number - 1, box_bounds.start + position);
+                    gears.entry(key).or_default().push(number);
+                }
+
+                if let Some(position) = contains_gear(&prev1[box_bounds.clone()]) {
+                    let key = (line_number, box_bounds.start + position);
+                    gears.entry(key).or_default().push(number);
+                }
+
+                if let Some(position) = contains_gear(&next[box_bounds.clone()]) {
+                    let key = (line_number + 1, box_bounds.start + position);
+                    gears.entry(key).or_default().push(number);
+                }
+            }
+        }
+
+        prev2 = prev1;
+        prev1 = next;
+    }
+
+    gears
+        .values()
+        .filter(|v| v.len() == 2)
+        .map(|v| v[0] * v[1])
+        .sum()
+}
